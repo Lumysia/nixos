25f252022f9fe5e5ee357c6e7be413525794b1b2
Livia Silver <sovranova@outlook.com>
2025-08-25 22:10:24 +0800

    fix: tailscale hostname

--- modules/nixos/services-tailscale.nix
+++ modules/nixos/services-tailscale.nix
@@ -6,7 +6,6 @@
     authKeyFile = "/etc/nixos/secrets/tailscale-authkey";
     extraUpFlags = [
       "--accept-dns"
-      "--hostname=homelab-cat-services" 
       "--snat-subnet-routes=false"
     ];
   };
