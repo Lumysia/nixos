908e39ca9ef2ecd69a37e6f6eae49e77d6c25ed0
Livia Silver <sovranova@outlook.com>
2025-08-25 11:43:24 +0800

    add: hardware config for services

---
+++ hosts/homelab-cat-services/hardware-configuration.nix
@@ -0,0 +1,22 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/profiles/qemu-guest.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "uhci_hcd" "ehci_pci" "ahci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/4b04ce32-a8ff-4636-a995-5c334fb99fe6";
+      fsType = "ext4";
+    };
+
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.ens18.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
