e281abecde398bfa2a6153c19d9b5dd74e8f169c
Livia Silver <sovranova@outlook.com>
2025-08-25 23:25:47 +0800

    fix: remove unnecessary config

--- modules/nixos/services-server.nix
+++ modules/nixos/services-server.nix
@@ -4,14 +4,4 @@
   services.openssh.enable = true;
   services.openssh.settings.PasswordAuthentication = false;
   services.qemuGuest.enable = true;
-
-  services.tailscale = {
-    enable = true;
-    authKeyFile = "/etc/nixos/secrets/tailscale-authkey";
-    extraUpFlags = [
-      "--accept-dns"
-      "--hostname=homelab-cat-core" 
-      "--snat-subnet-routes=false"
-    ];
-  };
 }
