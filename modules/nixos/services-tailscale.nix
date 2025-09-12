6340f2e8b56150789e6d281c27bb3805497ba97d
Lumysia <40706446+Lumysia@users.noreply.github.com>
2025-09-12 16:33:48 -0400

    fix: disable tailscale time-consuming check

--- modules/nixos/services-tailscale.nix
+++ modules/nixos/services-tailscale.nix
@@ -1,6 +1,13 @@
 { pkgs, ... }:
 
 {
+  nixpkgs.overlays = [
+    (final: prev: {
+      tailscale = prev.tailscale.overrideAttrs (oldAttrs: {
+        doCheck = false;
+      });
+    })
+  ];
   services.tailscale = {
     enable = true;
     authKeyFile = "/etc/nixos/secrets/tailscale-authkey";
